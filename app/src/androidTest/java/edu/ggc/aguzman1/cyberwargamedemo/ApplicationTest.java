b354bd2d1b96af8cee3d5bee13795e444bff234f
agguzman <a.guillermoguzman@gmail.com>
2016-01-19 20:05:57 -0500

    init

---
+++ app/src/androidTest/java/edu/ggc/aguzman1/cyberwargamedemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.ggc.aguzman1.cyberwargamedemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/edu/ggc/aguzman1/cyberwargamedemo/MapsActivity.java
@@ -0,0 +1,110 @@
+package edu.ggc.aguzman1.cyberwargamedemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        double lat = location.getLatitude() + 0.1;
+        double lng = location.getLongitude() + 0.1;
+
+        Toast.makeText(MapsActivity.this, "Checkpoint Lat: " + Double.toString(lat)
+                + "\nCheckpoint Lng: " + Double.toString(lng), Toast.LENGTH_SHORT).show();
+
+        LatLng checkpoint = new LatLng(lat, lng);
+
+        mMap.addMarker(new MarkerOptions().position(checkpoint).title("Checkpoint"));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        Toast.makeText(MapsActivity.this, "Lat: " + Double.toString(lat)
+                + "\nLng: " + Double.toString(lng), Toast.LENGTH_SHORT).show();
+
+        LatLng myLocation =  new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("You are here."));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 10));
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
